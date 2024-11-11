6955de2c32c4ad8207543be123b5be158fd0023b
iamsouane <ismailasouane08@gmail.com>
2024-11-11 22:58:52 +0000

    TD java 2016

---
+++ Exo1.java
@@ -0,0 +1,41 @@
+package TD2016;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Exo1 {
+    public static void main(String[] args) {
+        List<String> list = new LinkedList<>();
+        list.add("wade");
+        list.add("dabkh");
+        list.add("madji");
+        list.add("hrs");
+
+        // Affichage de la liste
+        System.out.println("Affichage de ma collection:");
+        for (String element : list) {
+            System.out.println(element);
+        }
+
+        // Recherche d'une chaîne
+        System.out.print("Entrez une chaîne : ");
+        Scanner clavier = new Scanner(System.in);
+        String chaine = clavier.nextLine();
+        boolean found = false;
+
+        for (String element : list) {
+            if (element.equals(chaine)) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            System.out.println("La chaîne est dans la liste");
+        } else {
+            System.out.println("La chaîne n'est pas dans la liste");
+        }
+        clavier.close();
+    }
+}
